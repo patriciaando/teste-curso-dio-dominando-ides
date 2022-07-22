43690c5d01724842fed9ea09a7ac1fe945e7e2aa
patriciaando <patricialeandroleite@gmail.com>
2022-07-21 22:19:03 -0300

    primeiro commit

---
+++ src/PrimeiroPrograma.java
@@ -0,0 +1,26 @@
+import model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato();
+		Livros livros = new Livros();
+		
+		System.out.println(gato);
+		System.out.println(livros);
+		
+		// TODO Auto-generated method stub
+		/*int a = 4;
+		int b = 5;
+		System.out.println("Hello Rafa " + (a*b));
+		*/
+	}
+
+}
+
+class Livros {
+	private String nome;
+	private String npag;
+	
+}
